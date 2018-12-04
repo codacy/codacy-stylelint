6a04d54a8d2fc10304bb434f3f4eee4488d523ea
Daniel Reigada <daniel@codacy.com>
2018-12-04 16:17:19 +0000

    Remove invalid patterns from documentation

--- src/main/scala/codacy/stylelint/DocGenerator.scala
+++ src/main/scala/codacy/stylelint/DocGenerator.scala
@@ -20,10 +20,10 @@ object DocGenerator {
     val rulesdir = tmpDirectory + "/lib/rules"
     val filePathForDocs = "src/main/resources/docs/"
 
-    val (folders, patterns) = getPatterns(rulesdir)
+    val patterns = getListOfSubDirectories(rulesdir)
     initializePatternsFile(patterns, version, filePathForDocs)
-    initializeDescriptionFile(folders, patterns, rulesdir, filePathForDocs)
-    copyDescriptionFiles(folders, rulesdir, filePathForDocs)
+    initializeDescriptionFile(patterns, rulesdir, filePathForDocs)
+    copyDescriptionFiles(patterns, rulesdir, filePathForDocs)
   }
 
   def cloneFromGitToTmpDir(tmpDirectory: better.files.File, version: String): Int = {
@@ -34,17 +34,6 @@ object DocGenerator {
   def getListOfSubDirectories(directoryName: String): List[String] = {
     val directory = File(directoryName)
     directory.walk(maxDepth = 1).filter(_.isDirectory).filterNot(_.isSamePathAs(directory)).map(_.name).toList
-  }
-
-  def getListOfFiles(directoryName: String, extensions: List[String]): List[String] = {
-    val directory = File(directoryName)
-    directory
-      .walk(maxDepth = 1)
-      .filter(_.isRegularFile)
-      .filterNot(_.nameWithoutExtension == "index")
-      .toList
-      .filter(file => extensions.exists(file.name.endsWith(_)))
-      .map(_.nameWithoutExtension)
   }
 
   def initializePatternsFile(patterns: List[String], version: String, filePathForDocs: String): File = {
@@ -69,28 +58,11 @@ object DocGenerator {
     Json.parse(stylelintConfigStandard).as[Map[String, Parameter.Value]]
   }
 
-  def getPatterns(rulesdir: String): (List[String], List[String]) = {
-    //get non-documented rules
-    val folders = getListOfSubDirectories(rulesdir)
-
-    //get non-documented rules(not enclosed in folders)
-    val okFileExtensions = List("js")
-    val patterns = getListOfFiles(rulesdir, okFileExtensions) ++ folders
-
-    (folders, patterns)
-  }
-
-  def initializeDescriptionFile(folders: List[String],
-                                patterns: List[String],
-                                rulesdir: String,
-                                filePathForDocs: String): File = {
+  def initializeDescriptionFile(patterns: List[String], rulesdir: String, filePathForDocs: String): File = {
 
     val patternsDescription: Set[Pattern.Description] = patterns.map { patternid =>
-      val patternDescription = if (folders contains patternid) {
+      val patternDescription =
         ParseMarkupRule.parseForDescriptions(File(rulesdir + "/" + patternid + "/README.md"))
-      } else {
-        patternid + ": further description not available"
-      }
       addNewDescription(patternid, patternDescription)
     }(collection.breakOut)
     File(filePathForDocs + "/description/description.json").write(Json.prettyPrint(Json.toJson(patternsDescription)))
