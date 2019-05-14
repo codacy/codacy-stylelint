2d3804f690277f54492f5ac49c73d684a876d41a
Tobias Weibel <2638420+tobiasweibel@users.noreply.github.com>
2019-05-14 12:05:40 +0200

    Update scss version and support recommended scss rules

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,8 @@ npm install -g stylelint-config-recommended@2.1.0
 npm install -g stylelint-order@0.8.1
 npm install -g stylelint-suitcss@3.0.0
 npm install -g stylelint-config-suitcss@14.0.0
-npm install -g stylelint-scss@3.2.0
+npm install -g stylelint-scss@3.6.1
+npm install -g stylelint-config-recommended-scss@3.3.0
 npm install -g stylelint-config-wordpress@13.0.0
 npm install -g stylelint-csstree-validator@1.3.0
 npm install -g stylelint-declaration-strict-value@1.0.4
