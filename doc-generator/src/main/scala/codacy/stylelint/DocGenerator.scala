1706dad1487694396bc79b4ed2a74722fce7a993
Stefan Vacareanu <stefan@codacy.com>
2024-10-17 14:11:44 +0300

    update doc-generator

--- doc-generator/src/main/scala/codacy/stylelint/DocGenerator.scala
+++ doc-generator/src/main/scala/codacy/stylelint/DocGenerator.scala
@@ -1,7 +1,6 @@
 package codacy.stylelint
 
 import java.nio.charset.Charset
-
 import better.files.{File, Resource}
 import com.codacy.plugins.api._
 import com.codacy.plugins.api.results.{Parameter, Pattern, Result, Tool}
@@ -13,48 +12,60 @@ object DocGenerator {
 
   def main(args: Array[String]): Unit = {
 
-    //val tempPluginsList =
-    listOfPlugins().map { plugin =>
-      processFolders(plugin.pluginName, plugin.url, plugin.relativeRulesDir)
-    }
-//
-//    val rulesDirs: List[String] = results.map(_._1)
-//
-//    val version = File("package.json").inputStream() { file =>
-//      Json.parse(file)("dependencies")("stylelint").as[String].stripPrefix("^")
-//    }
+    val pluginsList = processPlugins()
 
-//    val patterns = getListOfSubDirectories(rulesDirs) // refactor to accept list of directories
-//    initializePatternsFile(patterns, version)
-//    initializeDescriptionFile(patterns, rulesDirs)  // refactor to accept list of directories
-//    copyDescriptionFiles(patterns, rulesdirs, tmpDirectory, version)
+    initializePatternsFile(pluginsList)
+    initializeDescriptionFile(pluginsList)
+    copyDescriptionFiles(pluginsList)
   }
 
-  def processFolders(pluginName: String, url: String, relativeRulesDir: String): Unit = {
+  def processPlugins(): List[Plugin] = {
 
-    val tmpDirectory = File.newTemporaryDirectory()
-    val version = File("package.json").inputStream() { file =>
-      Json.parse(file)("dependencies")(pluginName).as[String].stripPrefix("^")
+    val pluginsList = listOfPlugins()
+    var tempList: List[Plugin] = List()
+
+    pluginsList.map { plugin =>
+      val version = File("package.json").inputStream() { file =>
+        Json.parse(file)("dependencies")(plugin.pluginName).as[String].stripPrefix("^")
+      }
+      val tmpDirectory = File.newTemporaryDirectory()
+      cloneFromGitToTmpDir(tmpDirectory, version, plugin.url)
+      val rulesdir = tmpDirectory + plugin.relativeRulesDir
+      val patterns = getListOfSubDirectories(rulesdir)
+      val tempPlugin =
+        Plugin(plugin.pluginName, plugin.relativeRulesDir, plugin.url, plugin.tree, patterns, version, tmpDirectory)
+
+      tempList = tempList :+ tempPlugin
     }
-    cloneFromGitToTmpDir(tmpDirectory, version, url)
-
-    val rulesdir = tmpDirectory + relativeRulesDir
-    val filePathForDocs = "/docs"
-// refactor
-    val patterns = getListOfSubDirectories(rulesdir)
-    initializePatternsFile(patterns, version, filePathForDocs)
-    initializeDescriptionFile(patterns, rulesdir, filePathForDocs)
-    copyDescriptionFiles(patterns, rulesdir, tmpDirectory, filePathForDocs, version)
-
+    (tempList)
   }
 
-  case class Plugin(pluginName: String, relativeRulesDir: String, url: String)
+  case class Plugin(pluginName: String,
+                    relativeRulesDir: String,
+                    url: String,
+                    tree: String,
+                    patterns: List[String],
+                    version: String,
+                    tempDirectory: better.files.File)
 
   def listOfPlugins(): List[Plugin] = {
-    List (
-      Plugin("stylelint", "/lib/rules", "https://github.com/stylelint/stylelint.git"),
-      Plugin("stylelint-a11y", "/src/rules", "https://github.com/YozhikM/stylelint-a11y.git")
-    )
+    List(
+      Plugin(
+        "stylelint",
+        "/lib/rules",
+        "https://github.com/stylelint/stylelint.git",
+        "https://github.com/stylelint/stylelint",
+        null,
+        null,
+        null),
+      Plugin(
+        "stylelint-a11y",
+        "/src/rules",
+        "https://github.com/YozhikM/stylelint-a11y.git",
+        "https://github.com/YozhikM/stylelint-a11y",
+        null,
+        null,
+        null))
   }
 
   def cloneFromGitToTmpDir(tmpDirectory: better.files.File, version: String, url: String): Int = {
@@ -73,14 +84,20 @@ object DocGenerator {
       .toList
   }
 
-  def initializePatternsFile(patterns: List[String], version: String, filePathForDocs: String): File = {
+  def initializePatternsFile(plugins: List[Plugin]): File = {
     val default = PatternsFromDefaultConfig()
+
+    val version = File("package.json").inputStream() { file =>
+      Json.parse(file)("dependencies")("stylelint").as[String].stripPrefix("^")
+    }
+
+    val patterns: List[String] = plugins.flatMap(_.patterns)
 
     val toolpatterns: Set[Pattern.Specification] = patterns.view.map { patternid =>
       addNewPattern(patternid, default.getOrElse(patternid, Parameter.Value(JsNull)))
     }.to(Set)
     val tool = Tool.Specification(Tool.Name("stylelint"), Option(Tool.Version(version)), toolpatterns)
-    File(filePathForDocs + "/patterns.json").write(Json.prettyPrint(Json.toJson(tool)))
+    File("/docs/patterns.json").write(Json.prettyPrint(Json.toJson(tool)))
   }
 
   def addNewPattern(patternName: String, default: Parameter.Value): Pattern.Specification = {
@@ -97,18 +114,23 @@ object DocGenerator {
     Json.parse(stylelintConfigStandard).as[Map[String, Parameter.Value]]
   }
 
-  def initializeDescriptionFile(patterns: List[String], rulesdir: String, filePathForDocs: String): File = {
+  def initializeDescriptionFile(plugins: List[Plugin]): File = {
 
-    val patternsDescription: Set[Pattern.Description] = patterns.view.map { patternid =>
-      val patternDescription =
-        ParseMarkupRule.parseForDescriptions(File(rulesdir + "/" + patternid + "/README.md"))
+    var finalPatternsDescription: Set[Pattern.Description] = Set()
+    plugins.map { plugin =>
+      val patternsDescription: Set[Pattern.Description] = plugin.patterns.map { pattern =>
+        val patternDescription =
+          ParseMarkupRule.parseForDescriptions(File(plugin.relativeRulesDir + "/" + pattern + "/README.md"))
 
-      // looking for markdown links, e.g., [text](https://www.example.com)
-      val urlRegex = """\[(.+?)\]\((.+?)\)""".r
-      val descriptionWithoutUrl = urlRegex.replaceAllIn(patternDescription, m => m.group(1)).trim
-      addNewDescription(patternid, descriptionWithoutUrl)
-    }.to(Set)
-    File(filePathForDocs + "/description/description.json").write(Json.prettyPrint(Json.toJson(patternsDescription)))
+        // looking for markdown links, e.g., [text](https://www.example.com)
+        val urlRegex = """\[(.+?)\]\((.+?)\)""".r
+        val descriptionWithoutUrl = urlRegex.replaceAllIn(patternDescription, m => m.group(1)).trim
+        addNewDescription(pattern, descriptionWithoutUrl)
+      }.to(Set)
+
+      finalPatternsDescription ++= patternsDescription
+    }
+    File("docs/description/description.json").write(Json.prettyPrint(Json.toJson(finalPatternsDescription)))
   }
 
   def addNewDescription(patternName: String, patternDescription: String): Pattern.Description = {
@@ -117,30 +139,29 @@ object DocGenerator {
     Pattern.Description(Pattern.Id(patternName), Pattern.Title(patternDescription), None, None, param)
   }
 
-  def copyDescriptionFiles(folderNames: List[String],
-                           rulesDirectory: String,
-                           mainDirectory: File,
-                           filePathForDocs: String,
-                           version: String): Unit = {
-    val descriptionDir = File(filePathForDocs + "/description/")
+  def copyDescriptionFiles(plugins: List[Plugin]): Unit = {
+    val descriptionDir = File("docs/description/")
     descriptionDir.createDirectories()
-    folderNames.foreach { patternName =>
-      val documentationFile = File(s"$rulesDirectory/$patternName/README.md")
-      val fileContent = documentationFile.contentAsString
+    plugins.map { plugin =>
+      plugin.patterns.foreach { patternName =>
+        val documentationFile = File(s"${plugin.relativeRulesDir}/$patternName/README.md")
+        val fileContent = documentationFile.contentAsString
 
-      // looking for markdown link to local resources, e.g, [`fix` option](../../../docs/user-guide/usage/options.md#fix)
-      // assuming local URLs start with "../" this is the pattern used at the time of this solution
-      val localUrlRegex = """\[(.+?)\]\((\.\./.+?)\)""".r
+        // looking for markdown link to local resources, e.g, [`fix` option](../../../docs/user-guide/usage/options.md#fix)
+        // assuming local URLs start with "../" this is the pattern used at the time of this solution
+        val localUrlRegex = """\[(.+?)\]\((\.\./.+?)\)""".r
 
-      val   contentWithReplacedUrls = localUrlRegex.replaceAllIn(fileContent, m => {
-        val linkText = m.group(1)
-        val localUrl = m.group(2)
-        val absoluteFilePath = documentationFile.parent / localUrl
-        val relativePath = mainDirectory.relativize(absoluteFilePath).toString
-        s"[$linkText](https://github.com/stylelint/stylelint/tree/$version/$relativePath)"
-      })
+        val contentWithReplacedUrls = localUrlRegex.replaceAllIn(fileContent, m => {
+          val linkText = m.group(1)
+          val localUrl = m.group(2)
+          val absoluteFilePath = documentationFile.parent / localUrl
+          val relativePath = plugin.tempDirectory.relativize(absoluteFilePath).toString
+          s"[$linkText](${plugin.tree}/${plugin.version}/$relativePath)"
+        })
 
-      File(s"$descriptionDir/$patternName.md").write(contentWithReplacedUrls)
+        File(s"$descriptionDir/$patternName.md").write(contentWithReplacedUrls)
+      }
     }
+
   }
 }
